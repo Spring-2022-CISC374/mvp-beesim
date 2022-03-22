d3a43ba78b0762662503ee7ed0d41480f1ab2b58
chrisark <chrisark@udel.edu>
2022-03-22 02:18:39 -0400

    Health bar and error message when hitting bear

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -15,19 +15,34 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
-        this.load.spritesheet('dude', 'assets/dude.png', {
-            frameWidth: 32, frameHeight: 48
+        this.load.spritesheet('dude', 'assets/bee.png', {
+            frameWidth: 128, frameHeight: 96
         })
             
         this.load.image('ground', 'assets/platform.png')
         this.load.image('bear', 'assets/Bear.png')
         this.load.image('sky', 'assets/sky.png')
         
+
+        this.load.image('left-cap', 'assets/barHorizontal_green_left.png')
+        this.load.image('middle', 'assets/barHorizontal_green_mid.png')
+        this.load.image('right-cap', 'assets/barHorizontal_green_right.png')
+
+        this.load.image('left-cap-shadow', 'assets/barHorizontal_shadow_left.png')
+        this.load.image('middle-shadow', 'assets/barHorizontal_shadow_mid.png')
+        this.load.image('right-shadow', 'assets/barHorizontal_shadow_right.png')
+
+
     }
 
     create()
     {
+        const fullWidth = 300
+        
+
         this.add.image(400,300,'sky')
+        this.add.text(10, 12, 'Energy')
+        this.createBarBackground(10, 50, fullWidth)
         this.platforms = this.physics.add.staticGroup()
         const ground = this.platforms.create(400,580, 'ground') as Phaser.Physics.Arcade.Sprite
         ground.setScale(2).refreshBody()
@@ -43,7 +58,7 @@ export default class HelloWorldScene extends Phaser.Scene
         this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude', { 
-                start: 0, end: 3
+                start: 0
 
             }),
             frameRate: 10,
@@ -53,7 +68,7 @@ export default class HelloWorldScene extends Phaser.Scene
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', {
-                start: 5, end: 8
+                start: 1
             }),
             frameRate: 10,
             repeat: -1
@@ -64,6 +79,28 @@ export default class HelloWorldScene extends Phaser.Scene
 
         this.keys = this.input.keyboard.createCursorKeys()
 
+        this.physics.add.overlap(this.player, this.enemy, this.handleHitEnemy, undefined, this)
+    
+        this.scene.launch('ui-scene', { controller: this})
+
+
+        
+    }
+
+    private handleHitEnemy(player: Phaser.GameObjects.GameObject, b: Phaser.GameObjects.GameObject) {
+        this.add.text(300, 200, 'Ouch')
+    }
+
+    private createBarBackground(x: number, y: number, fullWidth: number) {
+        const leftShadowCap = this.add.image(x, y, 'left-cap')
+            .setOrigin(0, 0.5)
+
+        const middleShadowCap = this.add.image(leftShadowCap.x + leftShadowCap.width, y, 'middle')
+            .setOrigin(0, 0.5)
+            middleShadowCap.displayWidth = fullWidth
+
+        this.add.image(middleShadowCap.x + middleShadowCap.displayWidth, y, 'right-cap')
+            .setOrigin(0, 0.5)
     }
     
     update() {
---
+++ src/scenes/UIScene.ts
@@ -0,0 +1,36 @@
+import Phaser from 'phaser'
+import HelloWorldScene from './HelloWorldScene';
+
+
+export default class UIScene extends Phaser.Scene
+{
+    constructor(){
+        super('ui-scene')
+    }
+
+    create() {
+        const x = 40
+        const y = 10
+        const fullWidth = 300
+
+        this.add.text(x, y - 36, 'Energy')
+
+        this.createBarBackground(x, y, fullWidth)
+
+
+
+    }
+        private createBarBackground(x: number, y: number, fullWidth: number) {
+            const leftShadowCap = this.add.image(x, y, 'left-cap-shadow')
+                .setOrigin(0, 0.5)
+
+            const middleShadowCap = this.add.image(leftShadowCap.x + leftShadowCap.width, y, 'middle-shadow')
+                .setOrigin(0, 0.5)
+                middleShadowCap.displayWidth = fullWidth
+
+            this.add.image(middleShadowCap.x + middleShadowCap.displayWidth, y, 'right-shadow-cap')
+                .setOrigin(0, 0.5)
+                    
+    }
+    
+}
