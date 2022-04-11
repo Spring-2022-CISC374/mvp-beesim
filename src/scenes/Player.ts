08a06789935143b9c6753695bc4870a0de89944e
trevorkoenig <tkoenig@udel.edu>
2022-04-11 13:39:32 -0400

    added flight

--- src/scenes/Player.ts
+++ src/scenes/Player.ts
@@ -5,19 +5,19 @@ export function generatePlayer(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprit
     player.setBounce(0.0);
     player.setScale(0.20);
     player.setCollideWorldBounds(true)
+    player.setMaxVelocity(250);
     return player;
 }
 
-export function beeController(keys?: Phaser.Types.Input.Keyboard.CursorKeys, player?: Phaser.Physics.Arcade.Sprite): void {
-    if(!keys || !player){
-        return;
+export function beeController(keys?: Phaser.Types.Input.Keyboard.CursorKeys, player?: Phaser.Physics.Arcade.Sprite, canFly: boolean): boolean {
+    if(!keys || !player) {
+        return false;
     }
-    if(keys.left?.isDown) 
-    {
+    if(keys.left?.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
     } 
-    else if (keys.right?.isDown){
+    else if (keys.right?.isDown) {
         player.setVelocityX(160);
         player.anims.play('right', true);
     }
@@ -25,8 +25,15 @@ export function beeController(keys?: Phaser.Types.Input.Keyboard.CursorKeys, pla
         player.setVelocityX(0);
     }
 
-    if (keys.up?.isDown && player.body.touching.down)
-    {
-        player?.setVelocityY(-330);
+    if (keys.up?.isDown) {
+        if (player.body.touching.down) {
+            player?.setVelocityY(-200);
+            return true;
+        } else {
+            player?.setAccelerationY(-500);
+        }
+    }
+    else {
+        player?.setAccelerationY(0);
     }
 }
