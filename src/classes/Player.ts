4c00026abc2e0e561348d91c6ceed7218780155c
Trevor <tkoenig@udel.edu>
2022-04-14 02:26:21 -0400

    turned it into a class and added health

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,42 +1,52 @@
 import Phaser from 'phaser';
+import HelloWorldScene from '~/scenes/HelloWorldScene';
 
-export function generatePlayer(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprite {
-    let player: Phaser.Physics.Arcade.Sprite =  scene.physics.add.sprite(100,450,'bee');
-    player.setBounce(0.0);
-    player.setGravityY(500);
-    player.setScale(0.20);
-    player.setCollideWorldBounds(true)
-    player.setMaxVelocity(250);
-    return player;
-}
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    public health: number;
 
-/*
-* Controls the bee
-*/
-export function beeController(keys?: Phaser.Types.Input.Keyboard.CursorKeys, player?: Phaser.Physics.Arcade.Sprite): void {
-    if(!keys || !player) {
-        return;
-    }
-    if(keys.left?.isDown) {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
-    } 
-    else if (keys.right?.isDown) {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    }
-    else {
-        player.setVelocityX(0);
+    constructor(scene: Phaser.Scene, x: number, y: number, sprite: string | Phaser.Textures.Texture){
+        super(scene, x, y, 'bee');
+        this.health = 6;
     }
 
-    if (keys.up?.isDown) {
-        if (player.body.touching.down) {
-            player?.setVelocityY(-200);
-        } else {
-            player?.setAccelerationY(-1500);
+    export function generatePlayer(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprite {
+        let player: Phaser.Physics.Arcade.Sprite =  new Player(scene, 100,450,'bee');
+        player.setBounce(0.0);
+        player.setGravityY(500);
+        player.setScale(0.20);
+        player.setCollideWorldBounds(true)
+        player.setMaxVelocity(250);
+        return player;
+    }
+
+    /*
+    * Controls the bee
+    */
+    export function beeController(keys?: Phaser.Types.Input.Keyboard.CursorKeys, player?: Phaser.Physics.Arcade.Sprite): void {
+        if(!keys || !player) {
+            return;
         }
-    }
-    else {
-        player?.setAccelerationY(0);
+        if(keys.left?.isDown) {
+            player.setVelocityX(-160);
+            player.anims.play('left', true);
+        } 
+        else if (keys.right?.isDown) {
+            player.setVelocityX(160);
+            player.anims.play('right', true);
+        }
+        else {
+            player.setVelocityX(0);
+        }
+
+        if (keys.up?.isDown) {
+            if (player.body.touching.down) {
+                player?.setVelocityY(-200);
+            } else {
+                player?.setAccelerationY(-1500);
+            }
+        }
+        else {
+            player?.setAccelerationY(0);
+        }
     }
 }
